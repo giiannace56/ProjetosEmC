bdad36fb4798d7873e155c6befe888ff823eefd9
giovanni iannace <giovanniiannace56@gmail.com>
2020-11-12 18:44:06 -0300

    finalizado

---
+++ projeto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro numero: ");
+            
+            int nm1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo numero: ");
+
+            int nm2 = int.Parse(Console.ReadLine());
+
+            int soma = nm1 + nm2 ;
+
+            Console.WriteLine("RESULTADO= " + soma) ;
+        }
+    }
+}
